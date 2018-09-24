e46dab06530d099ba12ecbdfdf24f78f8e2ffe4a
Remy Masson <muryoh@gmail.com>
2018-09-24 14:30:02 +0200

    Upgrade the build
    - Upgrade gradle to 4.10.2 and fix kotlin-dsl changes
    - Add bintray configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    kotlin("jvm") version "1.2.0"
+    kotlin("jvm") version "1.2.61"
     `maven-publish`
+    id("com.jfrog.bintray").version("1.8.4")
 }
 
 dependencies {
@@ -14,10 +15,37 @@ repositories {
     jcenter()
 }
 
+val sourcesJar by tasks.creating(Jar::class) {
+    classifier = "sources"
+    from(sourceSets["main"].java)
+}
+
 publishing {
-    (publications) {
-        "mavenJava"(MavenPublication::class) {
+    publications {
+        create("mavenJava", MavenPublication::class.java) {
             from(components["java"])
+            artifact(sourcesJar)
         }
     }
 }
+
+bintray {
+    user = project.findProperty("bintrayUser")?.toString()
+    key = project.findProperty("bintrayApiKey")?.toString()
+    setPublications("mavenJava")
+    publish = true
+    pkg.apply {
+        version.gpg.sign = true
+        userOrg = "ullink"
+        repo = "generic"
+        name = project.name
+        description = project.description
+        setLabels("junit", "test", "spek")
+        userOrg = "bintray_user"
+        setLicenses("Apache-2.0")
+        websiteUrl = "https://github.com/Ullink/${project.name}"
+        vcsUrl = "https://github.com/Ullink/${project.name}"
+        issueTrackerUrl = "https://github.com/Ullink/${project.name}/issues"
+    }
+}
+
